7f4419b645a2f878c4047f34313aef142bc05bdb
Srihari-Asus <srihari23052007@gmail.com>
2026-07-12 16:29:22 +0530

    Delete quiz-management directory

--- quiz-management/src/main/java/com/examly/springapp/repository/AnalyticsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.examly.springapp.repository;
-
-import com.examly.springapp.model.Analytics;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AnalyticsRepository extends JpaRepository<Analytics, Long> {
-}
--- quiz-management/src/main/java/com/examly/springapp/repository/EnrollmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.examly.springapp.repository;
-
-import com.examly.springapp.model.Enrollment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
-}
